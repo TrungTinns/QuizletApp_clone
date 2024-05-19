94452e1be066722bf9b576a2f3b7bb42f20bfcfe
neit1803 <tien18032002@gmail.com>
2024-05-19 09:32:46 +0700

    Detailed 3

--- lib/model/quiz/quiz.dart
+++ lib/model/quiz/quiz.dart
@@ -1,3 +1,17 @@
 class Quiz {
+<<<<<<< Updated upstream
+=======
+  String id;
+  String category;
+  String question;
+  String correct_answer;
+  List<String> incorrect_answer;
+  List<String> tags;
+  String difficulty;
+
+
+  Quiz ({required this.id, required this.category, required this.question, required this.correct_answer, required this.incorrect_answer, required this.tags, required this.difficulty});
+
+>>>>>>> Stashed changes
   
 }
